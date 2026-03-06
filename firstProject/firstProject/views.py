0c93211b43a186711c103ce645d09ac1524ff47a
Mahamudul Hasan Mehedi <mahamudul.hasan.mehedi.01@gmail.com>
2026-03-06 22:24:29 +0600

    Add views, templates, and static files for homepage, about, and contact pages

---
+++ firstProject/firstProject/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    #return HttpResponse("Hello, World!")
+    return render(request, 'index.html')
+
+def about(request):
+    return HttpResponse("This is the about page.")
+
+def contact(request):
+    return HttpResponse("This is the contact page.")
